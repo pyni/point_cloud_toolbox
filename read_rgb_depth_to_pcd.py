a95fd1002b5cfed3b1b57f297b0361bca204bb24
pyni <1208336359@qq.com>
2021-01-12 22:55:34 +0800

    Update read_rgb_depth_to_pcd.py

--- read_rgb_depth_to_pcd.py
+++ read_rgb_depth_to_pcd.py
@@ -8,9 +8,13 @@ if __name__=="__main__":
     depth_raw = o3d.io.read_image("/home/yuan/doc/objectpose/iros20-6d-pose-tracking/data/YCB_Video_Dataset/data_organized/0048/0depth.png")
 
     rgbd = geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw, convert_rgb_to_intensity = False);
-    pcd =  geometry.PointCloud.create_from_rgbd_image(rgbd, o3d.camera.PinholeCameraIntrinsic(
-        o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))                                                   
-    pcd.transform([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]])
+   # pcd =  geometry.PointCloud.create_from_rgbd_image(rgbd, o3d.camera.PinholeCameraIntrinsic(
+        #o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))                                                   
+#   # pcd.transform([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]])
+
+
+    pcd =  geometry.PointCloud.create_from_rgbd_image(rgbd, o3d.camera.PinholeCameraIntrinsic(1920, 1080, 1399.6968994140625, 1399.6971435546875, 943.8760375976562, 543.6397094726562))  #width,height,fx, fy, cx, cy                                                 
+   # pcd.transform([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]])
 
  
     io.write_point_cloud('./pc_color.pcd', pcd)
